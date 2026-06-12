228717db1bce36f5ccddc6c19798389278d29d27
Ramirez Mauro <mauroamaya58@gmail.com>
2026-06-12 00:46:46 -0300

    Add DOM ordering tests + improve setup.js MockElement querySelector/insertAdjacentElement/innerHTML
    - 5 DOM ordering tests verifying chronological element order
      in asstDiv (reasoning, content, tool-calls)
    - setup.js MockElement now supports [data-id], tag, and class
      querySelector; proper insertAdjacentElement (beforebegin,
      afterend); innerHTML parsing for <summary>/<div> children

--- tests/setup.js
+++ tests/setup.js
@@ -6,7 +6,7 @@ class MockElement {
     this.className = '';
     this.children = [];
     this.dataset = {};
-    this.innerHTML = '';
+    this._innerHTML = '';
     this._textContent = '';
     this.style = {};
     this.onclick = null;
@@ -16,11 +16,105 @@ class MockElement {
   get textContent() { return this._textContent; }
   set textContent(v) { this._textContent = v; }
   get outerHTML() { return '<' + this.tagName + '>'; }
-  appendChild(child) { this.children.push(child); return child; }
-  insertAdjacentElement(pos, el) { this.children.push(el); return el; }
-  remove() {}
-  querySelector(sel) { return null; }
-  querySelectorAll(sel) { return []; }
+  appendChild(child) { this.children.push(child); child.parentNode = this; return child; }
+  insertAdjacentElement(pos, el) {
+    if (pos === 'beforebegin' && this.parentNode) {
+      var p = this.parentNode;
+      var idx = p.children.indexOf(this);
+      if (idx >= 0) p.children.splice(idx, 0, el);
+      else p.children.push(el);
+      el.parentNode = p;
+    } else if (pos === 'afterend' && this.parentNode) {
+      var p = this.parentNode;
+      var idx = p.children.indexOf(this);
+      if (idx >= 0) p.children.splice(idx + 1, 0, el);
+      else p.children.push(el);
+      el.parentNode = p;
+    } else {
+      this.children.push(el);
+      el.parentNode = this;
+    }
+    return el;
+  }
+  remove() {
+    if (this.parentNode) {
+      var idx = this.parentNode.children.indexOf(this);
+      if (idx >= 0) this.parentNode.children.splice(idx, 1);
+    }
+  }
+  get innerHTML() { return this._innerHTML; }
+  set innerHTML(html) {
+    this._innerHTML = html;
+    this.children = [];
+    var tagRe = /<(\w+)([^>]*)>([^<]*)<\/\1>/g;
+    var attrRe = /(\w+)(?:="([^"]*)")?/g;
+    var m;
+    while ((m = tagRe.exec(html)) !== null) {
+      var child = new MockElement(m[1]);
+      var attrs = m[2];
+      var text = m[3];
+      var am;
+      while ((am = attrRe.exec(attrs)) !== null) {
+        if (am[1] === 'class' && am[2]) child.className = am[2];
+        else if (am[2] !== undefined) child.setAttribute(am[1], am[2]);
+      }
+      if (text && text.trim()) child.textContent = text.trim();
+      this.children.push(child);
+      child.parentNode = this;
+    }
+  }
+  querySelector(sel) {
+    // [data-xxx="val"]
+    var attrMatch = sel.match(/^\[data-(\w+)="([^"]*)"\]$/);
+    if (attrMatch) {
+      var attr = attrMatch[1], val = attrMatch[2];
+      function search(node) {
+        if (node.getAttribute('data-' + attr) === val || node.dataset[attr] === val) return node;
+        for (var i = 0; i < node.children.length; i++) {
+          var f = search(node.children[i]);
+          if (f) return f;
+        }
+        return null;
+      }
+      return search(this);
+    }
+    if (sel.indexOf('.') === 0) {
+      var cls = sel.substring(1);
+      for (var i = 0; i < this.children.length; i++) {
+        var c = this.children[i];
+        if (c.className === cls || c.className.indexOf(cls + ' ') === 0 || c.className.indexOf(' ' + cls) >= 0) return c;
+        var r = c.querySelector(sel);
+        if (r) return r;
+      }
+      return null;
+    }
+    if (sel === 'summary' || sel === 'details' || sel === 'div' || sel === 'span') {
+      var tag = sel.toUpperCase();
+      for (var i = 0; i < this.children.length; i++) {
+        if (this.children[i].tagName === tag) return this.children[i];
+        var r = this.children[i].querySelector(sel);
+        if (r) return r;
+      }
+      return null;
+    }
+    return null;
+  }
+  querySelectorAll(sel) {
+    if (sel.indexOf('.') === 0) {
+      var cls = sel.substring(1);
+      var res = [];
+      function walk(node) {
+        for (var i = 0; i < node.children.length; i++) {
+          var c = node.children[i];
+          if (c.className === cls || c.className.indexOf(cls + ' ') === 0 || c.className.indexOf(' ' + cls) >= 0) res.push(c);
+          walk(c);
+        }
+      }
+      walk(this);
+      return res;
+    }
+    return [];
+  }
   getAttribute(name) { return this.attributes[name] || this.dataset[name] || null; }
   setAttribute(name, val) { this.attributes[name] = val; }
   closest(sel) { return null; }
